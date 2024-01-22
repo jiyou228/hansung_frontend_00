89d146f6c2f6c45488a6fb7f2e2a1df7f634ef09
hey-juicy0 <2091177@hansung.ac.kr>
2024-01-22 14:17:58 +0900

    Merge branch 'main' of d

--- jobhak_front/src/components/Home.js
+++ jobhak_front/src/components/Home.js
@@ -1,10 +1,9 @@
 import Nav from './Nav';
-import './Home.css';
 const Home = () => {
   return(
-    <div className='home'>
+    <>
         <Nav/>
-    </div>
+    </>
   ); 
 };
 export default Home;
