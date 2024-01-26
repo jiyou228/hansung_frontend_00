25d70f2085497cdd2532993ed8c839dfec45515f
hey-juicy0 <143888533+hey-juicy0@users.noreply.github.com>
2024-01-27 05:35:46 +0900

    0127

--- jobhak_front/src/components/ReviseResume.js
+++ jobhak_front/src/components/ReviseResume.js
@@ -1,83 +1,11 @@
-// import React, { useState } from "react";
-// import Nav from './Nav';
-// import '../components/Resume.css';
-// const Resume = () => {
-//     const [inputText, setInputText] = useState("");
-//     const [inputCount, setInputCount] = useState(0);
-//     const [outputText, setOutputText] = useState("hi");
-
-//     const onChangeInput = (e) => {
-//         const text = e.target.value;
-//         setInputText(text);
-//         setInputCount(text.length);
-//         if(inputCount >= 998) {
-//             alert("최대 1000자 이내로 입력하세요!");
-//         }
-//     };
-
-//     const onReset = (e) => {
-//         e.preventDefault();
-//         setInputText("");
-//         setInputCount(0);
-//     };
-
-//     const submitResume = (e) => {
-//         e.preventDefault();
-//         setOutputText("bye");
-//     };
-
-//     return (
-//         <div className="resume">
-//             <Nav />
-//             <div className="resume_app" style={{ textAlign: 'center', justifyContent: 'center', margin: 'auto', fontFamily: 'NanumSquare'}}>
-//                 <p>AI 첨삭 자기소개서</p>
-//                 <form onSubmit={submitResume}>
-//                     <div style={{height: 'fit-content', width: 'fit-content', textAlign: 'center', justifyContent: 'center', margin: 'auto'}}>
-//                     <p>글자수: {inputCount}</p>
-//                     <textarea
-//                         maxLength="1000"
-//                         placeholder="자기소개서를 작성해보세요. (최대 1000자)"
-//                         style={{ overflow: 'auto', height: '40vh', width: '72vw', wordBreak: 'breakAll', whiteSpace: 'normal',padding: '1rem', resize: 'none', fontFamily: 'NanumSquare' }}
-//                         value={inputText}
-//                         onChange={onChangeInput}
-//                     />
-                    
-//                     </div>
-//                     <div>
-//                         <button onClick={onReset} style={{backgroundColor: '#104085', color: 'white', border: 'none', fontSize: '1.1rem', width: '6rem', padding: '0.2rem 0', borderRadius: '0.2rem',fontFamily: 'NanumSquare', fontWeight: '700'}}>초기화</button>
-//                         <button type="submit" style={{marginLeft: '2rem',backgroundColor: '#104085', color: 'white', border: 'none', fontSize: '1.1rem', width: '6rem', padding: '0.2rem 0', borderRadius: '0.2rem',fontFamily: 'NanumSquare', fontWeight: '700'}}>분석</button>
-//                     </div>
-//                     <textarea
-//                         style={{ height: '40vh', width: '72vw', padding: '1rem', resize: 'none', backgroundColor: 'pink', fontFamily: 'NanumSquare' }}
-//                         value={outputText}
-//                         readOnly
-//                     />
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Resume;
-
-import React, { useState } from "react";
+/* import React, { useState } from "react";
 import Nav from './Nav';
-import './Resume.css';
-import { Link } from "react-router-dom";
-
+import '../components/Resume.css';
 const Resume = () => {
     const [inputText, setInputText] = useState("");
     const [inputCount, setInputCount] = useState(0);
     const [outputText, setOutputText] = useState("hi");
-    const [clipboardValue, setClipboardValue] = useState('');
-    const pasteClipboard = async() => {
-        try{
-            const clipText = await navigator.clipboard.readText();
-            setClipboardValue(clipText);
-        } catch(error){
-            console.error(`붙여넣기에 실패했습니다:\n ${error}`);
-        }
-    };
+
     const onChangeInput = (e) => {
         const text = e.target.value;
         setInputText(text);
@@ -99,46 +27,113 @@ const Resume = () => {
     };
 
     return (
-        <div className="revise_app">
-            <Nav/>
-            <div style={{width: '70vw', margin:'auto'}}>
-                <ul style={{listStyle: 'none', display:'flex', gap: '3rem', fontSize: '1.5rem', lineHeight: '1.5rem'}}>
-                    <Link to ='/resume/write' style={{textDecoration: 'none', color: '#252525'}}>
-                        <li>AI 작성 자소서</li>
-                    </Link>
-                    <Link to ='/resume/revise' style={{textDecoration: 'none', color: '#252525', borderBottom: '5px solid blue', height: '3rem', fontWeight: '700'}}>
-                        <li>AI 첨삭 자소서</li>
-                    </Link>
-                </ul>
-                <hr style={{position: 'relative', top: '-1.7rem', border: '1px solid gray', zIndex:'-100', opacity: '50%'}}/>
-            </div>
-            <div className="revise" style={{ fontFamily: 'NanumSquare', margin: 'auto', textAlign:'center'}}>
-                <h2>AI 첨삭 자기소개서</h2>
+        <div className="resume">
+            <Nav />
+            <div className="resume_app" style={{ textAlign: 'center', justifyContent: 'center', margin: 'auto', fontFamily: 'NanumSquare'}}>
+                <p>AI 첨삭 자기소개서</p>
                 <form onSubmit={submitResume}>
-                    <div style={{height: 'fit-content', width: '100vw', textAlign:'center'}}>
+                    <div style={{height: 'fit-content', width: 'fit-content', textAlign: 'center', justifyContent: 'center', margin: 'auto'}}>
                     <p>글자수: {inputCount}</p>
                     <textarea
                         maxLength="1000"
-                        placeholder="자기소개서를 작성해보세요. (최대 1000자)"
-                        style={{fontSize: '1.15rem', overflow: 'auto', height: '40vh', width: '32vw', wordBreak: 'breakAll', whiteSpace: 'normal',padding: '1rem', resize: 'none', fontFamily: 'NanumSquare' }}
+                        placeholder="자기소개서를 작성해보세요. (최대 1000자)"4C8EED
+                        style={{ overflow: 'auto', height: '40vh', width: '72vw', wordBreak: 'breakAll', whiteSpace: 'normal',padding: '1rem', resize: 'none', fontFamily: 'NanumSquare' }}
                         value={inputText}
                         onChange={onChangeInput}
                     />
+                    
+                    </div>
+                    <div>
+                        <button onClick={onReset} style={{backgroundColor: '#104085', color: 'white', border: 'none', fontSize: '1.1rem', width: '6rem', padding: '0.2rem 0', borderRadius: '0.2rem',fontFamily: 'NanumSquare', fontWeight: '700'}}>초기화</button>
+                        <button type="submit" style={{marginLeft: '2rem',backgroundColor: '#104085', color: 'white', border: 'none', fontSize: '1.1rem', width: '6rem', padding: '0.2rem 0', borderRadius: '0.2rem',fontFamily: 'NanumSquare', fontWeight: '700'}}>분석</button>
+                    </div>
                     <textarea
-                        style={{fontSize: '1.15rem', height: '40vh', width: '32vw', padding: '1rem', resize: 'none', backgroundColor: 'pink',marginLeft: '3rem',ontFamily: 'NanumSquare' }}
+                        style={{ height: '40vh', width: '72vw', padding: '1rem', resize: 'none', backgroundColor: 'pink', fontFamily: 'NanumSquare' }}
                         value={outputText}
                         readOnly
                     />
-                    </div>
-                    <div style={{marginTop: '0.2rem', textAlign: 'center'}}>
-                    <button onClick={onReset} style={{backgroundColor: '#104085', color: 'white', border: 'none', fontSize: '1.1rem', width: '6rem', padding: '0.2rem 0', borderRadius: '0.2rem',fontFamily: 'NanumSquare', fontWeight: '700'}}>초기화</button>
-                        <button type="submit" style={{marginTop: '2rem',marginLeft: '1rem',backgroundColor: '#104085', color: 'white', border: 'none', fontSize: '1.1rem', width: '6rem', padding: '0.2rem 0', borderRadius: '0.2rem',fontFamily: 'NanumSquare', fontWeight: '700'}}>분석</button>
-                    </div>
                 </form>
-                <input type="text" value={clipboardValue} onChange={(e) => setClipboardValue(e.target.value)} />
-                <button onClick={pasteClipboard}>Paste from Clipboard</button>
             </div>
         </div>
+    );
+};
+
+export default Resume; */
+
+import React, { useState } from "react";
+import Nav from './Nav';
+import './ReviseResume.css';
+import { Link } from "react-router-dom";
+
+const Resume = () => {
+    const [inputText, setInputText] = useState("");
+    const [inputCount, setInputCount] = useState(0);
+    const [outputText, setOutputText] = useState("하단의 분석 버튼을 누르면 피드백 내용이 보입니다.");
+    
+    const onChangeInput = (e) => {
+        const text = e.target.value;
+        setInputText(text);
+        setInputCount(text.length);
+        if(inputCount >= 998) {
+            alert("최대 1000자 이내로 입력하세요!");
+        }
+    };
+
+    const onReset = (e) => {
+        e.preventDefault();
+        alert('\n입력한 내용이 모두 삭제됩니다. 정말로 초기화하시겠습니까? \n\n"확인"을 누르시면 초기화됩니다. ');
+        setInputText("");
+        setInputCount(0);
+    };
+
+    const submitResume = (e) => {
+        e.preventDefault();
+        setOutputText("bye");
+    };
+
+    return (
+        <>
+            <Nav/>
+            <div className="revise_container">
+                <ul className="revise_list">
+                    <Link to ='/resume/write' className="revise_link">
+                        <li>AI 작성 자소서</li>
+                    </Link>
+                    <Link to ='/resume/revise' className="revise_link_active">
+                        <li>AI 첨삭 자소서</li>
+                    </Link>
+                </ul>
+                <hr className="revise_hr"/>
+            </div>
+            <div className="revise_title">AI 첨삭 자기소개서</div>
+                <form onSubmit={submitResume} className="revise_form">
+                <div className="revise_count">글자수: {inputCount}</div>
+                    <div className="revise_content">
+                    <div className="revise_inputarea">
+                        <h3>나의 자기소개서</h3>
+                    <textarea
+                        maxLength="1000"
+                        placeholder="자기소개서를 작성해보세요. (최대 1000자)"
+                        className="revise_input"
+                        value={inputText}
+                        onChange={onChangeInput}
+                    />
+                    </div>
+                    <div className="revise_outputarea">
+                        <h3>AI 피드백</h3>
+                    <textarea
+                        value={outputText}
+                        readOnly
+                        className="revise_output"
+                    />
+                    </div>
+                    </div>
+                    <div className="revise_button_container">
+                        <button onClick={onReset} className="revise_button_reset">초기화</button>
+                        <button type="submit" className="revise_button_next">분석</button>
+                    </div>
+                </form>
+        </>
     );
 };
 
--- jobhak_front/src/components/WriteResume.js
+++ jobhak_front/src/components/WriteResume.js
@@ -1,48 +1,68 @@
-import React from "react";
-import './WriteResume.js';
+import React , {useState} from "react";
+import './WriteResume.css';
 import Nav from "./Nav.js";
 import { Link } from "react-router-dom";
 
-const writeResume = () => {
-    return(
+const WriteResume = () => {
+    const [clipboardValue, setClipboardValue] = useState('');
+    const pasteClipboard = async() => {
+        try{
+            const clipText = await navigator.clipboard.readText();
+            setClipboardValue(clipText);
+        } catch(error){
+            console.error(`붙여넣기에 실패했습니다:\n ${error}`);
+        }
+    };
+    const submitWrite = () =>{
+
+    }
+    const onReset = (e) =>{
+        e.preventDefault();
+        alert('\n입력한 내용이 모두 삭제됩니다. 정말로 초기화하시겠습니까? \n\n"확인"을 누르시면 초기화됩니다. ');
+        window.location.reload();
+    }
+    return (
         <>
-            <Nav/>
-            <div style={{width: '70vw', margin:'auto'}}>
-                <ul style={{listStyle: 'none', display:'flex', gap: '3rem', fontSize: '1.5rem', lineHeight: '1.5rem'}}>
-                    <Link to ='/resume/write' style={{textDecoration: 'none', color: '#252525', borderBottom: '5px solid blue', height: '3rem', fontWeight: '700'}}>
+            <Nav />
+            <div className="write_container"> 
+                <ul className="write_list">
+                    <Link to='/resume/write' className="write_link_active"> 
                         <li>AI 작성 자소서</li>
                     </Link>
-                    <Link to ='/resume/revise' style={{textDecoration: 'none', color: '#252525'}}>
+                    <Link to='/resume/revise' className="write_link"> 
                         <li>AI 첨삭 자소서</li>
                     </Link>
                 </ul>
-                <hr style={{position: 'relative', top: '-1.7rem', border: '1px solid gray', zIndex:'-100', opacity: '50%'}}/>
+                <hr className="write_hr" />
             </div>
-                <form style={{overflowY: 'scroll', maxHeight: '75vh', marginTop: '1.5rem'}}>
-                    <div style={{width: '70vw', margin: 'auto', fontFamily:"NanumSquare"}}>
-                        <div style={{border: '1.5px solid black', height: '3rem', lineHeight: '3rem', borderRadius: '10px'}}>
-                            <label style={{paddingLeft: '1rem', fontSize: '1.2rem', fontWeight: '700'}}>지원하는 회사명</label>
-                            <input type="text" required style={{width: '88%', border:'none', marginLeft:'1rem', height: '2rem', lineHeight: '2rem'}}></input>
-                        </div>
-                        <div style={{border: '1.5px solid black', height: '3rem', lineHeight: '3rem', borderRadius: '10px', marginTop: '2rem'}}>
-                            <label style={{paddingLeft: '1rem', fontSize: '1.2rem', fontWeight: '700'}}>지원하는 직무/직군</label>
-                            <input type="text"  required style={{width: '85%', border:'none', marginLeft:'1rem', height: '2rem', lineHeight: '2rem'}}></input>
-                        </div>
-                        <div style={{border: '1.5px solid black', borderRadius: '10px', marginTop: '2rem', paddingTop: '1rem'}}>
-                            <label style={{paddingLeft: '1rem', fontSize: '1.2rem', fontWeight: '700'}}>채용공고 본문</label>
-                            <button style={{border: 'none', backgroundColor: '#104085', fontFamily:"NanumSquare", color: 'white', width: '8rem', height: '2.3rem', marginLeft: '1rem', borderRadius: '5px', fontSize: '1.2rem', lineHeight: '2.3rem', padding: '0'}}>붙여넣기</button>
-                            <textarea placeholder="이 부분을 추가하시면 지원하는 기업에 맞는 자기소개서가 작성됩니다!" style={{ height: '60vh', width: '97%', padding: '1rem', resize: 'none', marginLeft: '0.25rem', marginTop: '1rem'}} />
-                        </div>
-                        <div style={{border: '1.5px solid black', borderRadius: '10px', marginTop: '2rem', paddingTop: '1rem'}}>
-                            <label style={{paddingLeft: '1rem', fontSize: '1.2rem', fontWeight: '700'}}>관련 경험 또는 이력</label>
-                            <textarea placeholder="Your placeholder text" style={{ height: '20vh', width: '97%', padding: '1rem', resize: 'none', marginLeft: '0.25rem', marginTop: '1rem'}} />
-                        </div>
+            <div className="write_title">AI 작성 자기소개서</div>
+            <form className="write_form" onSubmit={submitWrite}>
+                <div className="write_content">
+                    <div className="write_input_container">
+                        <label className="write_label">지원하는 회사명 * </label>
+                        <input type="text" className="write_input" required placeholder="필수) 예시: 삼성전자" />
                     </div>
-                    <div style={{textAlign: 'center', marginTop: '1rem'}}>
-                    <button style={{textAlign: 'center', border: 'none', backgroundColor: '#104085', fontFamily:"NanumSquare", color: 'white', width: '8rem',  borderRadius: '5px', fontSize: '1.5rem',  padding: '0.5rem ', marginBottom: '5rem'}}>다음</button>
+                    <div className="write_input_container">
+                        <label className="write_label">지원하는 직무/직군 * </label>
+                        <input type="text" className="write_input" required placeholder="필수) 예시: 마케팅 (Customer Marketing CRM)" />
                     </div>
-                </form>
+                    <div className="write_input_container">
+                        <label className="write_label">채용공고 본문</label>
+                        <button className="write_button" onClick={pasteClipboard}>붙여넣기</button>
+                        <textarea className="write_textarea" value={clipboardValue} onChange={(e) => setClipboardValue(e.target.value)} placeholder="이 부분을 추가하시면 지원하는 기업에 맞는 자기소개서가 작성됩니다!" />
+                    </div>
+                    <div className="write_input_container">
+                        <label className="write_label">관련 경험 또는 이력</label>
+                        <textarea className="write_textarea" placeholder="이 부분을 추가하시면 더 좋은 자기소개서가 작성됩니다." />
+                    </div>
+                </div>
+                <div className="write_button_container">
+                    <button className="write_button_reset" onClick={onReset}>초기화</button>
+                    <button type= "submit" className="write_button_next">다음</button>
+                </div>
+            </form>
         </>
     );
 };
-export default writeResume;
+
+export default WriteResume;
