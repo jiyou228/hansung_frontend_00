f7673be1a9129590d1e24f70b5252a18ca1dc9d8
hey-juicy0 <2091177@hansung.ac.kr>
2024-01-30 14:30:14 +0900

    app.js 수정본

--- jobhak_front/src/App.js
+++ jobhak_front/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import { LoginProvider } from "./components/LoginContext";
+import {LoginProvider} from './components/LoginContext';
 import Signup from "./components/Signup";
 import JoinCheck from "./components/JoinCheck";
 import Login from "./components/Login";
@@ -15,10 +15,6 @@ import Profile from "./components/Profile";
 import WriteResume from "./components/WriteResume";
 import Home from "./components/Home";
 import ReviseResume from "./components/ReviseResume";
-import Guide from "./components/Guide";
-import CountChar from "./components/CountChar";
-import Sidebar from "./components/Side_bar";
-
 function App() {
   return (
     <LoginProvider>
@@ -38,10 +34,6 @@ function App() {
           <Route path="/profile/female" element={<Profile />}></Route>
           <Route path="/resume/write" element={<WriteResume />}></Route>
           <Route path="/resume/revise" element={<ReviseResume />}></Route>
-          <Route path="/resume" element={<Resume />}></Route>
-          <Route path="/guide" element={<Guide />} />
-          <Route path="/countchar" element={<CountChar />} />
-          <Route path="/sidebar" element={<Sidebar />} />
         </Routes>
       </BrowserRouter>
     </LoginProvider>
