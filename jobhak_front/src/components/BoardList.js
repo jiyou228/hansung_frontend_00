0d94fe503875267db66901e0c5c987d1e9dd9ded
hey-juicy0 <2091177@hansung.ac.kr>
2024-02-20 14:12:47 +0900

    0220-board

--- jobhak_front/src/components/BoardList.js
+++ jobhak_front/src/components/BoardList.js
@@ -7,6 +7,7 @@ import category from '../assets/categories.png';
 import filter from '../assets/filter.png';
 import search from '../assets/search.png'
 import prev from '../assets/previous.png';
+import profile from '../assets/profile.png';
 import next from '../assets/next.png';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -15,13 +16,18 @@ import axios from "axios";
 
 const BoardList = () => {
     const [isfilter, setisFilter] = useState(false);
-
+    const [bookmarks, setBookmarks] = useState([]);
     const [boardList, setBoardList] = useState([
-    { id: 1, title: '게시물 1', content: '게시물 내용 1' },
-    { id: 2, title: '게시물 2', content: '게시물 내용 2' },
-    { id: 3, title: '게시물 3', content: '게시물 내용 3' },
-    { id: 4, title: '게시물 4', content: '게시물 내용 4' },
-    { id: 5, title: '게시물 5', content: '게시물 내용 5' }
+    { postId: 1, title: '게시물 1', content: '게시물 내용 1', replies: 100},
+    { postId: 2, title: '게시물 2', content: '게시물 내용 2', replies: 100 },
+    { postId: 3, title: '게시물 3', content: '게시물 내용 3', replies: 100 },
+    { postId: 4, title: '게시물 4', content: '게시물 내용 4', replies: 100 },
+    { postId: 5, title: '게시물 5', content: '게시물 내용 5', replies: 100 },
+    { postId: 6, title: '게시물 6', content: '게시물 내용 6', replies: 100 },
+    { postId: 7, title: '게시물 7', content: '게시물 내용 7', replies: 100 },
+    { postId: 8, title: '게시물 8', content: '게시물 내용 8', replies: 100 },
+    { postId: 9, title: '게시물 9', content: '게시물 내용 9', replies: 100 },
+    { postId: 10, title: '게시물 10', content: '게시물 내용 10', replies: 100 },
   ]);
   const [bestPosts, setBestPosts] = useState([
     {id: 1, title: '인기글 1'},
@@ -65,25 +71,43 @@ const BoardList = () => {
     .catch(err => {
       console.error('에러 발생: ', err);
     })
+    axios.get('/user/bookmark')
+    .then(res => {
+      setBookmarks(res.data);
+    })
+    .catch(err => {
+      console.error('에러 발생: ', err);
+    });
+    setBookmarks([1,3,5]);
   }, [page, sortOption, selectedCategory]);
 
   return(
     <>
     <Nav/>
    <div className="boardlist_app">
-        <div className="boardlist_best">
-            <h2>Best 인기글🔥</h2>
-            <ul>
-            {bestPosts.map(post => (
-            <li key={post.id}>
-                <Link to={`/board/${post.id}`}>{post.title}</Link>
-            </li>
-            ))}
-        </ul>
+    <div className="boardlist_top">
+    <div className="boardlist_profile">
+        <img src = {profile} alt="프사"/>
+        <div className="boardlist_profileDetail">
+        <p><b>잡학이</b>님</p>
+          <strong>글 수: </strong>100 
+          <strong style={{paddingLeft: '1vw'}}>댓글 수:</strong>200
         </div>
+      </div>
+      <div className="boardlist_best">
+              <h2>Best 인기글🔥</h2>
+              <ul>
+              {bestPosts.map(post => (
+              <li key={post.postId}>
+                  <Link to={`/board/${post.postId}`}>{post.title}</Link>
+              </li>
+              ))}
+          </ul>
+      </div>
+    </div>
         <div className="boardlist_filter">
             <img onClick={openFilter} src ={filter} alt="필터"/>
-            <button >게시글 작성</button>
+            <Link to ='/boardlist/write'><button>게시글 작성</button></Link>
         </div>
         {isfilter && 
                 <div className="modal_filter">
@@ -103,28 +127,31 @@ const BoardList = () => {
                 <img src = {category} alt="분류"/>
                 <select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
                     <option selected>전체</option>
-                    <option>이력서</option>
-                    <option>면접</option>
-                    <option>정보 교환</option>
+                    <option value="resume">이력서</option>
+                    <option value="interview">면접</option>
+                    <option value="share">정보 교환</option>
                 </select>
                 </div>
                 </div>
             </div>}
         <div className="boardlist_list">
         {boardList.map(board => (
-        <div key={board.id} className="boardlist_post">
+        <div key={board.postId} className="boardlist_post">
             <div className="boardlist_title">
-            <Link to={`/boardlist/detail/${board.id}`}>
+            <Link to={`/boardlist/detail/${board.postId}`}>
               {board.title}
               </Link>
-              <img src={off_bookmark} alt="북마크"/>
-
+              {bookmarks.includes(board.postId) ? (
+                <img src={on_bookmark} alt="북마크" />
+            ) : (
+                <img src={off_bookmark} alt="북마크" />
+            )}
             </div>
             <div className="boardlist_content">
             {board.content.substring(0, 40)}
               <div className="boardlist_comment">
               <img src={comment} alt="댓글"/>
-              <label>몇개</label>
+              <label>{board.replies}</label>
                 </div>
             </div>
         </div>
