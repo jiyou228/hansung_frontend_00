402f768d74c236c1a0a6ca114f78fe92d065c444
Jeewoo Yim <2091177@hansung.ac.kr>
2024-04-29 15:48:25 +0900

    커밋

--- jobhak_front/src/setupTests.js
+++ jobhak_front/src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import "@testing-library/jest-dom/extend-expect";
+import "regenerator-runtime/runtime";
+
+//Fix for "matchMedia not present, legacy browsers require a polyfill jest" error
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+    };
+  };
