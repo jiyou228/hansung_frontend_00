f7de3426fb81448ea26e596ee0b60a8876d4569c
Jeewoo Yim <2091177@hansung.ac.kr>
2024-05-10 14:52:23 +0900

    login

--- jobhak_front/src/components/Login.js
+++ jobhak_front/src/components/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
   const submitLogin = (e) => {
     e.preventDefault();
     axios
-      .post("https://jobhakdasik.site/", {
+      .post("https://jobhakdasik.site:8080/", {
         loginId: id,
         password: pw,
       })
