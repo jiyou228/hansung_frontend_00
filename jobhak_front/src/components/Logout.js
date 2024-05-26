7f4379de88227378fd9ce6aa5f60f12f7624a37d
Jeewoo Yim <2091177@hansung.ac.kr>
2024-05-26 18:35:49 +0900

    aboutGAN

--- jobhak_front/src/components/Logout.js
+++ jobhak_front/src/components/Logout.js
@@ -4,37 +4,41 @@ import { useNavigate } from "react-router-dom";
 import instance from "../axiosConfig";
 
 const Logout = () => {
-  const [cookie, , removeCookie] = useCookies();
+  const [, , removeCookie] = useCookies();
   const navigate = useNavigate();
 
   useEffect(() => {
+    const performLogout = () => {
+      // 쿠키 삭제
+      removeCookie("loggedIn", { path: "/" });
+      removeCookie("loginModal", { path: "/" });
+      removeCookie("loginId", { path: "/" });
+      removeCookie("user_id", { path: "/" });
+      removeCookie("nickname", { path: "/" });
+      removeCookie("MyIMG", { path: "/" });
+      removeCookie("provider", { path: "/" });
+      removeCookie("refreshToken", { path: "/", domain: ".jobhakdasik.site" });
+
+      // 로컬 스토리지에서 토큰 제거
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("revision");
+
+      // 로그아웃 후 홈페이지로 이동
+      navigate("/");
+    };
+
+    // 로그아웃 요청
     instance
-      .get("https://api.jobhakdasik.site/logout")
+      .post("/logout")
       .then((res) => {
-        console.log(res);
-        console.log(cookie.refreshToken);
-        // 쿠키 삭제
-        removeCookie("loggedIn", { path: "/" });
-        removeCookie("loginModal", { path: "/" });
-        removeCookie("loginId", { path: "/" });
-        removeCookie("user_id", { path: "/" });
-        removeCookie("nickname", { path: "/" });
-        removeCookie("MyIMG", { path: "/" });
-        removeCookie("provider", { path: "/" });
-
-        removeCookie("refreshToken", { path: "/" , domain: '.jobhakdasik.site'});
+        console.log("로그아웃 성공:", res);
+        performLogout();
         alert("로그아웃이 완료되었습니다.");
-        console.log("쿠키 삭제 완료");
-
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("revision");
-        // 로그아웃 후 홈페이지로 이동
-        navigate("/");
       })
       .catch((err) => {
-        console.error("에러 발생: ", err);
-        alert("로그아웃 도중 에러가 발생했습니다. 새로고침");
-        window.location.reload();
+        console.error("로그아웃 에러 발생: ", err);
+        alert("로그아웃 도중 에러가 발생했습니다. 새로고침 해주세요.");
+        performLogout();
       });
   }, [removeCookie, navigate]);
 
